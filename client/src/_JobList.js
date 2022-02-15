946e103fbf17068da49e24ed19cfecd29a8134f1
Gianni Nola <53386620+Gnola@users.noreply.github.com>
2022-02-15 14:47:22 -0800

    Added filtering functionality

--- client/src/_JobList.js
+++ client/src/_JobList.js
@@ -7,13 +7,37 @@ export const JobList = ({ jobs }) => {
 
   // const { jobs } = useContext(GlobalContext);
   const [clicked, setClicked] = useState('all')
+  const [filteredJobs, setFilteredJobs] = useState(jobs)
+
+  const handleClick = (text) => {
+    switch (text) {
+      case 'all':
+        setClicked('all')
+        setFilteredJobs(jobs)
+        break;
+      case 'potential':
+        setClicked('potential')
+        setFilteredJobs(jobs.filter(job => job.status === 'Potential'))
+        break;
+      case 'active':
+        setClicked('active')
+        setFilteredJobs(jobs.filter(job => job.status !== 'Rejected'))
+        break;
+      case 'done':
+        setClicked('done')
+        setFilteredJobs(jobs.filter(job => job.status == 'Rejected'))
+        break;
+      default:
+        setFilteredJobs(jobs)
+    }
+  }
 
   return (
     <>
-      <button type='button' className={clicked === 'all' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>setClicked('all')}>All</button>
-      <button type='button' className={clicked === 'potential' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>setClicked('potential')}>Potential</button>
-      <button type='button' className={clicked === 'active' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>setClicked('active')}>Active</button>
-      <button type='button' className={clicked === 'done' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>setClicked('done')}>Done</button>
+      <button type='button' className={clicked === 'all' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>handleClick('all')}>All</button>
+      <button type='button' className={clicked === 'potential' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>handleClick('potential')}>Potential</button>
+      <button type='button' className={clicked === 'active' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>handleClick('active')}>Active</button>
+      <button type='button' className={clicked === 'done' ? 'btn btn-primary' : '"btn btn-light"'} onClick={()=>handleClick('done')}>Done</button>
       {clicked === 'all' &&(<h1>All</h1>)}
       {clicked === 'potential' &&(<h1>Potential</h1>)}
       {clicked === 'active' &&(<h1>Active</h1>)}
@@ -30,9 +54,12 @@ export const JobList = ({ jobs }) => {
           </tr>
         </thead>
         <tbody>
-          {jobs.map((job) => (
-              <Job key={job.id} job={job}/>
-          ))}
+          {
+            filteredJobs.length === 0 ?
+            jobs.map((job) => (<Job key={job.id} job={job}/>))
+            :
+            filteredJobs.map((job) => (<Job key={job.id} job={job}/>))
+          }
         </tbody>
       </table>
     </>
