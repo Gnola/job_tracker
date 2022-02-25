807328625e4a17f29fc415e4dcdb77256247d1e2
Gianni Nola <53386620+Gnola@users.noreply.github.com>
2022-02-25 14:24:17 -0800

    Working on company data

--- client/src/components/_Company.js
+++ client/src/components/_Company.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { IoCloseSharp } from "react-icons/io5";
+import { IoCloseSharp, IoSettingsOutline } from "react-icons/io5";
 
  class Company extends React.Component {
 
    state = {
      job:this.props.job,
-     editing:false,
-     view: 'info'
+     view: 'info',
+     requirements:'',
+     salary:'',
+
    }
 
    changeView = (view) => {
@@ -17,9 +19,12 @@ import { IoCloseSharp } from "react-icons/io5";
 
 
 
+
+
    render() {
-     const { job, editing, view } = this.state
+     const { job, view, requirements, salary } = this.state
      const { companyOpened, setCompanyOpened } = this.props
+     console.log(view);
 
      return (
        <div id='company-modal' className="modal" tabIndex="-1" role="dialog" style={{display:'block'}}>
@@ -27,6 +32,7 @@ import { IoCloseSharp } from "react-icons/io5";
            <div className="modal-content">
              <div className="modal-header">
                <h2 className="modal-title">{job.company}</h2>
+               <IoSettingsOutline className='icon' onClick={() => this.changeView('editing')}/>
                <IoCloseSharp className='icon' onClick={()=>setCompanyOpened(!companyOpened)}/>
              </div>
              <div className='company-buttons'>
@@ -62,6 +68,7 @@ import { IoCloseSharp } from "react-icons/io5";
                   </div>
                </div>
                :
+               view === 'resources' ?
                <div className='modal-resources'>
                   <div id='things-to-mention' className='modal-body-part'>
                     <h3>Things to Mention / Brag List</h3>
@@ -78,6 +85,52 @@ import { IoCloseSharp } from "react-icons/io5";
                     <p><strong>About the Process:</strong></p>
                   </div>
               </div>
+              :
+              <form>
+                  <div id='job-info' className='modal-body-part'>
+                  <h3>Job Info</h3>
+                    <label htmlFor="jobTitle"><strong>Job Title:</strong></label>
+                    <input type='text' value={job.jobTitle} />
+                    <label htmlFor="jobBoard"><strong>Job Board:</strong></label>
+                    <input type='text' value={job.jobBoard} />
+                    <label htmlFor="resume"><strong>Resume:</strong></label>
+                    <input type='text' value={job.resume} />
+                    <label htmlFor="connections"><strong>Connection(s):</strong></label>
+                    <input type='text' value={job.connections} />
+                    <label htmlFor="requirements"><strong>Requirements/Qualifications:</strong></label>
+                    <textarea type='text' value={requirements}  />
+                    <label htmlFor="salary"><strong>Salary:</strong></label>
+                    <input type='text' value={salary} />
+                  </div>
+                   <div id='company-info' className='modal-body-part'>
+                     <h3>Company Info</h3>
+                     <p><strong>Mission/Values:</strong></p>
+                     <p><strong>Team/Environment:</strong></p>
+                     <p><strong>Size:</strong></p>
+                     <p><strong>Years in Business:</strong></p>
+                     <p><strong>Category:</strong> {job.category}</p>
+                     <p><strong>Location(s):</strong> {job.location}</p>
+                   </div>
+                   <div id='interviewing-info' className='modal-body-part'>
+                     <h3>Interviewing Info</h3>
+                     <p><strong>Interviewer(s):</strong></p>
+                     <p><strong>Process:</strong></p>
+                   </div>
+                   <div id='things-to-mention' className='modal-body-part'>
+                     <h3>Things to Mention / Brag List</h3>
+                     <p><strong>Strengths:</strong></p>
+                     <p><strong>Weaknesses:</strong></p>
+                     <p><strong>Projects:</strong></p>
+                     <p><strong>Experience:</strong></p>
+                   </div>
+                   <div id='questions' className='modal-body-part'>
+                     <h3>Questions</h3>
+                     <p><strong>About the Company:</strong></p>
+                     <p><strong>About the Job/Role:</strong></p>
+                     <p><strong>About Me:</strong></p>
+                     <p><strong>About the Process:</strong></p>
+                   </div>
+             </form>
              }
              </div>
            </div>
